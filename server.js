426aeec180836546bfadc5799b0feeb44cc16c26
lawrenzi <lawrenzisamuel@gmail.com>
2016-10-08 14:32:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,8 +5,12 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-app.get('//', function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+app.get('/ui/article-one.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
